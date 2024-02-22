6f1d9c05667ed4d2638790fa180e800662924854
Tomas Tobar <codetombomb@gmail.com>
2024-02-22 14:13:37 -0600

    add vehicles controller

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,6 @@
 class Rental < ApplicationRecord
-  belongs_to :user, dependent: :destroy
-  belongs_to :vehicle, dependent: :destroy
+  belongs_to :user
+  belongs_to :vehicle
   has_one :rental_transaction
   alias_attribute :owner, :user
 end
